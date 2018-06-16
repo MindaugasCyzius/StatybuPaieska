953950ecfc54cd48b025a0af2a26eb87cb62d303
Mindaugas Čyžius <mindaugas.cyzius@gmail.com>
2018-06-16 20:07:13 +0300

    Pakeistas tekstas

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Statybu paieška namams</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Sveikas atvykęs į aplikacijos pradžią. Dabar viską sukodink :D
         </p>
       </div>
     );
